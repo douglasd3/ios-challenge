5d1dab0c0ed3c1ca9e81772fe51aeac930ab040c
Douglas Barbosa <douglasdb3@gmail.com>
2015-06-24 17:43:55 -0300

    Added PhotoObject class

---
+++ iOSChallenge/PhotoObject.h
@@ -0,0 +1,13 @@
+//
+//  PhotoObject.h
+//  iOSChallenge
+//
+//  Created by Douglas Barbosa on 24/06/15.
+//  Copyright (c) 2015 Douglas Barbosa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PhotoObject : NSObject
+
+@end
---
+++ iOSChallenge/PhotoObject.m
@@ -0,0 +1,13 @@
+//
+//  PhotoObject.m
+//  iOSChallenge
+//
+//  Created by Douglas Barbosa on 24/06/15.
+//  Copyright (c) 2015 Douglas Barbosa. All rights reserved.
+//
+
+#import "PhotoObject.h"
+
+@implementation PhotoObject
+
+@end
