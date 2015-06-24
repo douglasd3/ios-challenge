c178dee78cb8d3577b5db281e4697009f3f80833
Douglas Barbosa <douglasdb3@gmail.com>
2015-06-24 10:28:27 -0300

    Initial Commit

---
+++ iOSChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSChallenge
+//
+//  Created by Douglas Barbosa on 24/06/15.
+//  Copyright (c) 2015 Douglas Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSChallenge
+//
+//  Created by Douglas Barbosa on 24/06/15.
+//  Copyright (c) 2015 Douglas Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
