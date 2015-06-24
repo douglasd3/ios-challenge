dcf2e547c4fbb60e55dd2ed791f9dee9575cf120
Douglas Barbosa <douglasdb3@gmail.com>
2015-06-24 14:40:25 -0300

    Added RestAPIHandler class

---
+++ iOSChallenge/RestAPIHandler.m
@@ -0,0 +1,13 @@
+//
+//  RestAPIHandler.m
+//  iOSChallenge
+//
+//  Created by Douglas Barbosa on 24/06/15.
+//  Copyright (c) 2015 Douglas Barbosa. All rights reserved.
+//
+
+#import "RestAPIHandler.h"
+
+@implementation RestAPIHandler
+
+@end
